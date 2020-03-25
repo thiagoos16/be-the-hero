a00383714815d806045adc5da0cdc976fdfccc3f
thiagoos16 <tos@icomp.ufam.edu.br>
2020-03-25 11:14:49 -0400

    add delete and list incidents routes

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,8 @@ const IncidentController = require('./controllers/IncidentController');
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
+routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentsController.delete);
 
 module.exports = routes;
